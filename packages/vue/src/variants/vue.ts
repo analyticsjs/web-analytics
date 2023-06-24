d485018d2491a07a6afcee8d6fb551c1c28b0ffb
chengpeiquan <chengpeiquan@chengpeiquan.com>
2023-06-24 14:43:57 +0800

    chore(vue): adjust directory structure

---
+++ packages/vue/src/variants/vue.ts
@@ -0,0 +1,82 @@
+import { isBrowser } from '@bassist/utils'
+import { VueAnalytics } from '../analytics'
+import { getMajorVersion, getGlobalProperty } from '../utils'
+import type { Platform } from '@web-analytics/core'
+import type {
+  CreateVueAnalyticsInstanceOptions,
+  InstallerProviderOptions,
+  VueInstance,
+} from '../types'
+
+function installerProvider<P extends Platform>({
+  analytics,
+  platform,
+}: InstallerProviderOptions<P>) {
+  return {
+    install: (
+      app: VueInstance,
+      { router, ...others }: CreateVueAnalyticsInstanceOptions
+    ) => {
+      if (!isBrowser) return
+
+      // Synchronize the real properties with the prototype to the exported instance
+      const analyticsInstance = new VueAnalytics<P>({ platform, ...others })
+      for (const key in analyticsInstance) {
+        if (Object.prototype.hasOwnProperty.call(analyticsInstance, key)) {
+          // @ts-ignore
+          analytics[key] = analyticsInstance[key]
+        }
+      }
+      const prototype = Object.getPrototypeOf(analyticsInstance)
+      Object.setPrototypeOf(analytics, prototype)
+
+      // Add a global property to Vue instance
+      const property = getGlobalProperty(platform)
+      const version = getMajorVersion(app)
+      switch (version) {
+        case 2: {
+          app.prototype[property] = analytics
+          break
+        }
+        case 3: {
+          app.config.globalProperties[property] = analytics
+          break
+        }
+      }
+
+      // Automatic tracking of pageview
+      if (!router) return
+      router.afterEach(() => {
+        analytics.trackPageview({
+          pageUrl: window.location.href,
+        })
+      })
+    },
+  }
+}
+
+export function createVueBaiduAnalytics() {
+  // @ts-ignore
+  const baiduAnalytics: VueAnalytics<'baidu'> = {}
+
+  return {
+    baiduAnalytics,
+    installVueBaiduAnalytics: installerProvider({
+      analytics: baiduAnalytics,
+      platform: 'baidu',
+    }),
+  }
+}
+
+export function createVueCnzzAnalytics() {
+  // @ts-ignore
+  const cnzzAnalytics: VueAnalytics<'cnzz'> = {}
+
+  return {
+    cnzzAnalytics,
+    installVueCnzzAnalytics: installerProvider({
+      analytics: cnzzAnalytics,
+      platform: 'cnzz',
+    }),
+  }
+}
