392e633b47c03849124a25f984dd9527faccd780
chengpeiquan <chengpeiquan@chengpeiquan.com>
2023-06-26 14:03:27 +0800

    wip(examples): adjust deps and aliases

--- examples/vue2/vite.config.ts
+++ examples/vue2/vite.config.ts
@@ -1,7 +1,17 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [vue()],
-})
+  resolve:
+    command === 'build'
+      ? {}
+      : {
+          alias: {
+            '@web-analytics/vue': resolve(__dirname, '../../packages/vue/lib'),
+            '@web-analytics/shared': resolve(__dirname, '../shared'),
+          },
+        },
+}))
